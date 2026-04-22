9fc95b1bd279cc31e9f4b47d7485c8ae2d704b7e
nanacrackingthesecodes <dianakurochkina22@gmail.com>
2026-04-22 20:48:35 +0200

    4

--- src/components/Recomendation/Recomendation.js
+++ src/components/Recomendation/Recomendation.js
@@ -11,6 +11,7 @@ function Recomendation() {
     "НАЗВА НАБОРУ",
     "НАЗВА НАБОРУ",
   ];
+
   const bottomTags = [
     "НАЗВА НАБОРУ",
     "НАЗВА НАБОРУ",
@@ -21,6 +22,7 @@ function Recomendation() {
     "НАЗВА НАБОРУ",
     "НАЗВА НАБОРУ",
   ];
+
   const images = [
     "/recomendation1.jpg",
     "/recomendation2.jpg",
@@ -31,67 +33,76 @@ function Recomendation() {
   ];
 
   return (
-    <section className="recomendation">
-    <h2 className="recomendation-title">ДОДАТКОВІ РЕКОМЕНДАЦІЇ</h2>
-    <div className="recomendation-tags">
-      {topTags.map((tag, index) => (
-        <span className="recomendation-tag" key={index}>
-          {tag}
-        </span>
-      ))}
-    </div>
-    <div className="recomendation-grid">
-      <div className="recomendation-item">
-        <div className="recomendation-card recomendation-card-1">
-          <img src={images[0]} alt="Рекомендація 1" />
-          <div className="recomendation-arrow">↗</div>
-        </div>
-        <p className="recomendation-name">НАЗВА НАБОРУ</p>
+    <section className="recommendation">
+      <h2 className="recommendation-title">ДОДАТКОВІ РЕКОМЕНДАЦІЇ</h2>
+
+      <div className="recommendation-tags">
+        {topTags.map((tag, index) => (
+          <span className="recommendation-tag" key={index}>
+            {tag}
+          </span>
+        ))}
       </div>
-      <div className="recomendation-item">
-        <div className="recomendation-card recomendation-card-2">
-          <img src={images[1]} alt="Рекомендація 2" />
-          <div className="recomendation-arrow">↗</div>
+
+      <div className="recommendation-grid">
+        <div className="recommendation-item">
+          <div className="recommendation-card recommendation-card-1">
+            <img src={images[0]} alt="Рекомендація 1" />
+            <div className="recommendation-arrow">↗</div>
+          </div>
+          <p className="recommendation-name">НАЗВА НАБОРУ</p>
         </div>
-        <p className="recomendation-name">НАЗВА НАБОРУ</p>
-      </div>
-      <div className="recomendation-item">
-        <div className="recomendation-card recomendation-card-3">
-          <img src={images[2]} alt="Рекомендація 3" />
-          <div className="recomendation-arrow">↗</div>
+
+        <div className="recommendation-item">
+          <div className="recommendation-card recommendation-card-2">
+            <img src={images[1]} alt="Рекомендація 2" />
+            <div className="recommendation-arrow">↗</div>
+          </div>
+          <p className="recommendation-name">НАЗВА НАБОРУ</p>
         </div>
-        <p className="recomendation-name">НАЗВА НАБОРУ</p>
-      </div>
-      <div className="recomendation-item">
-        <div className="recomendation-card recomendation-card-4">
-          <img src={images[3]} alt="Рекомендація 4" />
-          <div className="recomendation-arrow">↗</div>
+
+        <div className="recommendation-item">
+          <div className="recommendation-card recommendation-card-3">
+            <img src={images[2]} alt="Рекомендація 3" />
+            <div className="recommendation-arrow">↗</div>
+          </div>
+          <p className="recommendation-name">НАЗВА НАБОРУ</p>
         </div>
-        <p className="recomendation-name">НАЗВА НАБОРУ</p>
-      </div>
-      <div className="recomendation-item recomendation-item-5">
-        <div className="recomendation-card recomendation-card-5">
-          <img src={images[4]} alt="Рекомендація 5" />
-          <div className="recomendation-arrow">↗</div>
+
+        <div className="recommendation-item">
+          <div className="recommendation-card recommendation-card-4">
+            <img src={images[3]} alt="Рекомендація 4" />
+            <div className="recommendation-arrow">↗</div>
+          </div>
+          <p className="recommendation-name">НАЗВА НАБОРУ</p>
         </div>
-        <p className="recomendation-name">НАЗВА НАБОРУ</p>
-      </div>
-      <div className="recomendation-item">
-        <div className="recomendation-card recomendation-card-6">
-          <img src={images[5]} alt="Рекомендація 6" />
-          <div className="recommndation-arrow">↗</div>
+
+        <div className="recommendation-item recommendation-item-5">
+          <div className="recommendation-card recommendation-card-5">
+            <img src={images[4]} alt="Рекомендація 5" />
+            <div className="recommendation-arrow">↗</div>
+          </div>
+          <p className="recommendation-name">НАЗВА НАБОРУ</p>
         </div>
-        <p className="recomendation-name">НАЗВА НАБОРУ</p>
+
+        <div className="recommendation-item">
+          <div className="recommendation-card recommendation-card-6">
+            <img src={images[5]} alt="Рекомендація 6" />
+            <div className="recommendation-arrow">↗</div>
+          </div>
+          <p className="recommendation-name">НАЗВА НАБОРУ</p>
+        </div>
       </div>
-    </div>
-    <div className="recomendation-tags recomendation-tags-bottom">
-      {bottomTags.map((tag, index) => (
-        <span className="recomendation-tag" key={index}>
-          {tag}
-        </span>
-      ))}
-    </div>
-  </section>
-);
+
+      <div className="recommendation-tags recommendation-tags-bottom">
+        {bottomTags.map((tag, index) => (
+          <span className="recommendation-tag" key={index}>
+            {tag}
+          </span>
+        ))}
+      </div>
+    </section>
+  );
 }
+
 export default Recomendation;
